3e303d46bbcca2d1eea27234a60cd9d1691ab92c
mseknibilel <msekni.bilel@gmail.com>
2018-06-22 23:25:38 +0200

    DEV | refactoring reservation form using input()/output()

--- resores/src/app/app.component.ts
+++ resores/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { Restaurant } from './services/restaurant.model';
+import { Observable } from 'rxjs';
+import { Table } from './services/table.model';
 
 @Component({
   selector: 'rr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Resores';
+  restaurants$: Observable<Restaurant[]>;
+  tables$: Observable<Table[]>;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.restaurants$ = this.apiService.getRestaurants();
+    this.tables$ = this.apiService.getTables();
+  }
+  onContinue(payload: { restaurant: string, table: string }): void {
+    this.apiService.saveReservation(payload);
+  }
 }
