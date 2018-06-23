bd60f4eb95b9a456c9ad94395db918ad1b9313f2
mseknibilel <msekni.bilel@gmail.com>
2018-06-23 11:47:52 +0200

    DEV | Added elements project

--- resores/projects/elements/src/app/app.module.ts
+++ resores/projects/elements/src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
 export class AppModule { }
