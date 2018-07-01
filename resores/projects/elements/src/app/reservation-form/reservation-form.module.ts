6902e4c883f9d55bbed95f4e54c5f0df00628445
mseknibilel <msekni.bilel@gmail.com>
2018-07-01 16:12:19 +0200

    Revert "DEV | Refactored to base reservation logic"
    This reverts commit 54e66d454e6996a92795a0576aef040135c0e9e2.

--- resores/projects/elements/src/app/reservation-form/reservation-form.module.ts
+++ resores/projects/elements/src/app/reservation-form/reservation-form.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from './reservation-form.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [
